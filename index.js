fa6eacbdcb1900335529a08c8132a5ca6742a740
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-05 21:49:00 -0400

    frst solution

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-//* Use Node to execute file
-// Developer Reminder -> npm link -> link file
+//* ^--Use Node to execute file
+// Developer Reminder -> npm link -> link filename
 
 //* File System From Node
 const fs = require('fs');
@@ -10,5 +10,26 @@ fs.readdir(process.cwd(), (err, filenames) => {
   if (err) {
     console.log(err);
   }
-  console.log(filenames);
+
+  // Get information about file
+  const allStats = Array(filenames.length).fill(null);
+  for (let filename of filenames) {
+    const index = filenames.indexOf(filename);
+    fs.lstat(filename, (err, stats) => {
+      if (err) {
+        console.log(err);
+      }
+      allStats[index] = stats;
+
+      const ready = allStats.every((stats) => {
+        return stats;
+      });
+
+      if (ready) {
+        allStats.forEach((stats, index) => {
+          console.log(filenames[index], stats.isFile());
+        });
+      }
+    });
+  }
 });
