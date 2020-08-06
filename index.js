a1b32e056d4e66ce58e7ee42e57570fa3801bdbe
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-05 22:04:32 -0400

    solution 2 wtih method 3 of promise

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
 #!/usr/bin/env node
-//* ^--Use Node to execute file
+//* ^-- Node to execute file
 // Developer Reminder -> npm link -> link filename
 
 //* File System From Node
 const fs = require('fs');
 const util = require('util');
 
-//* Method #2 with Promise
+// Method #2 with Promise
 // const lstat = util.promisify(fs.lstat);
 
-//* Method #3
+//* Method #3 with Promise
 const { lstat } = fs.promises;
 
 //* Read Current Directory
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(process.cwd(), async (err, filenames) => {
   if (err) {
     console.log(err);
   }
 
-  //* Get information about file and determine type
-
-  // Method #1 with Promise
-  // const lstat = (filename) => {
-  //   return new Promise((resolve, reject) => {
-  //     fs.lstat(filename, (err, stats) => {
-  //       if (err) {
-  //         reject(err);
-  //       }
-
-  //       resolve(stats);
-  //     });
-  //   });
-  // };
+  // Get information about file and determine type
+  for (let filename of filenames) {
+    const stats = await lstat(filename);
+    try {
+      console.log(filename, stats.isFile());
+    } catch (err) {
+      console.log(err);
+    }
+  }
 });
+
+// Method #1 with Promise
+// const lstat = (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err);
+//       }
+
+//       resolve(stats);
+//     });
+//   });
+// };
